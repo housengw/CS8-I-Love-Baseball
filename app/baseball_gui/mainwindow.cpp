0e13b0da5f6e0022e849a93d666f8dad8947aa25
jackground <jackground@icloud.com>
2020-05-05 14:10:01 -0700

    removed code for pixmap. Label property to rich text insert figure. map centered

--- app/baseball_gui/mainwindow.cpp
+++ app/baseball_gui/mainwindow.cpp
@@ -9,16 +9,6 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    QPixmap mapPixmap(":/map.jpg");                 // Pixelmap of the map
-
-    int width_label_map = ui->label_map->width();   // UI label_map item width
-    int height_label_map = ui->label_map->height(); // UI labl_map item height
-
-    // PROC - set ui label_map variable to QPixmap object defined above
-    //        as mapPixmap
-    // Documentation: https://doc.qt.io/qt-5/qpixmap.html#scaled
-    ui->label_map->setPixmap(mapPixmap.scaled(width_label_map,
-                                              height_label_map));
 }
 
 MainWindow::~MainWindow()
