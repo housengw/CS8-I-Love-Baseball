a2751cbd630fcae1c2677c5f73043bc0cfae905b
housengw <46389172+stevenhsw@users.noreply.github.com>
2020-05-06 23:28:51 -0700

    Merge pull request #20 from stevenhsw/Yi
    reopening Yi's pull request

--- src/baseball_project/stadium.cpp
+++ src/baseball_project/stadium.cpp
@@ -3,14 +3,33 @@
 Stadium::Stadium():
     _stadium_name(""),
     _team_name(""),
-    _address(Address()),
-    _phone_number(0),
+//    _address(Address()),
+    _address(""),
+
+    _phone_number(""),
     _date_opened(Date()),
-    _capacity(0)
+    _capacity(0),
+    _surface ("Grass")
 {
     //intentionally left empty
 }
 
+Stadium::Stadium (string stadium,
+                  string team,
+                  string adress,
+                  string phone,
+                  Date date,
+                  int capacity,
+                  string surface)
+{
+    set_stadium_name(stadium);
+    set_team_name(team);
+    set_address(adress);
+    set_phone_number(phone);
+    set_date_opened(date);
+    set_capacity(capacity);
+    set_surface (surface);
+}
 
 void Stadium::set_stadium_name(const string& stadium_name){
     _stadium_name = stadium_name;
@@ -22,12 +41,12 @@ void Stadium::set_team_name(const string& team_name){
 }
 
 
-void Stadium::set_address(const Address& address){
+void Stadium::set_address(const string& address){
     _address = address;
 }
 
 
-void Stadium::set_phone_number(const int& phone_number){
+void Stadium::set_phone_number(const string& phone_number){
     _phone_number = phone_number;
 }
 
@@ -39,5 +58,24 @@ void Stadium::set_date_opened(const Date& date_opened){
 
 void Stadium::set_capacity(const int& capacity){
     _capacity = capacity;
+}
+
+void  Stadium::set_surface(const string& surface)
+{
+    _surface = surface;
+}
+
+void Stadium::print_info ()
+{
+    cout << "Stadium : " << get_stadium_name() << endl;
+    cout << "Team Name: " << get_team_name() << endl;
+    cout << "Address: " <<endl<< get_address() << endl;
+    cout << "Opened Date: " << _date_opened.get_month() << "/" << _date_opened.get_day ()
+                     << "/" << _date_opened.get_year() << endl;
+    cout << "Phone: " << get_phone_number() << endl;
+
+    cout << "Capacity:" << get_capacity () << endl;
+    cout << "Surface: " << get_surface() << endl;
+
 }
 
