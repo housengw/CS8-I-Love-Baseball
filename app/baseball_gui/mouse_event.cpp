0bc5a06f5f6611bb532fbc439ec5b0dbe693b589
Yi-28 <64219919+Yi-28@users.noreply.github.com>
2020-05-11 13:45:00 -0700

    Locations (x,y coordiantes)
    - added mouse_event class
    - show location of mouse only inside the map
    - show location pressed in map

---
+++ app/baseball_gui/mouse_event.cpp
@@ -0,0 +1,17 @@
+#include "mouse_event.h"
+
+mouse_event::mouse_event(QWidget *parent) : QLabel(parent)
+{
+    setMouseTracking(true);
+}
+
+void mouse_event::mouseMoveEvent (QMouseEvent *ev)
+{
+    this->x = ev->x();
+    this->y = ev->y();
+    emit Mouse_Pos();
+}
+void mouse_event::mouseReleaseEvent (QMouseEvent *)
+{
+    emit Mouse_Release();
+}
